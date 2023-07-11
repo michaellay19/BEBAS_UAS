34540c8848d9f6f1a40b30ea10a0f865bbf3cff2
Hansen Tanio <hansentanio33@gmail.com>
2023-07-11 21:21:15 +0700

    Membuat Login dan Register Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:bebas_uas/halaman_utama.dart';
+import 'package:bebas_uas/login.dart';
+import 'package:bebas_uas/register.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -8,12 +10,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HalamanUtama(),
+      home: LoginPage(),
     );
   }
 }
---
+++ lib/register.dart
@@ -0,0 +1,217 @@
+import 'package:flutter/material.dart';
+import 'package:bebas_uas/login.dart';
+
+class RegisterPage extends StatefulWidget {
+  RegisterPage({super.key});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
+  final confirmController = TextEditingController();
+
+  bool? checkedValue = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        centerTitle: true,
+        title: Text(
+          "Sign Up",
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+        iconTheme: IconThemeData(
+          color: Colors.black,
+        ),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+        ),
+        child: Center(
+            child: Column(
+          children: [
+            SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              child: TextField(
+                controller: usernameController,
+                obscureText: false,
+                decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white)),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey.shade400),
+                    ),
+                    fillColor: Colors.grey[250],
+                    filled: true,
+                    hintText: 'Username',
+                    hintStyle: TextStyle(color: Colors.grey[500])),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              child: TextField(
+                controller: passwordController,
+                obscureText: true,
+                decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white)),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey.shade400),
+                    ),
+                    fillColor: Colors.grey[250],
+                    filled: true,
+                    hintText: 'Password',
+                    hintStyle: TextStyle(color: Colors.grey[500])),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              child: TextField(
+                controller: confirmController,
+                obscureText: true,
+                decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white)),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey.shade400),
+                    ),
+                    fillColor: Colors.grey[250],
+                    filled: true,
+                    hintText: 'Confirm Password',
+                    hintStyle: TextStyle(color: Colors.grey[500])),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            GestureDetector(
+              onTap: () {
+                if (usernameController.text == '' ||
+                    passwordController.text == '' ||
+                    confirmController.text == '') {
+                  showDialog(
+                    context: context,
+                    builder: (context) =>
+                        errorDialog(context, 'Fill all textfield'),
+                  );
+                } else if (passwordController.text != confirmController.text) {
+                  showDialog(
+                    context: context,
+                    builder: (context) =>
+                        errorDialog(context, 'Password are not the same'),
+                  );
+                } else {
+                  showDialog(
+                    context: context,
+                    builder: (context) => successDialog(context),
+                  );
+                }
+              },
+              child: Container(
+                padding: const EdgeInsets.all(20),
+                margin: const EdgeInsets.symmetric(horizontal: 30),
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Center(
+                  child: Text(
+                    'Sign Up',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 17,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  'Already have an account?',
+                  style: TextStyle(fontSize: 16),
+                ),
+                TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => LoginPage(),
+                      ),
+                    );
+                  },
+                  child: const Text(
+                    'Login',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.black,
+                      decoration: TextDecoration.underline,
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ],
+        )),
+      ),
+    );
+  }
+}
+
+errorDialog(BuildContext context, String msg) {
+  return AlertDialog(
+    title: const Text('Sign Up Error!'),
+    content: Text(msg),
+    actions: [
+      TextButton(
+        onPressed: () {
+          Navigator.pop(context);
+        },
+        child: const Text('OK'),
+      )
+    ],
+  );
+}
+
+successDialog(BuildContext context) {
+  return AlertDialog(
+    title: const Text('Sign Up Success!'),
+    content: const Text('Data has been saved'),
+    actions: [
+      TextButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => LoginPage(),
+            ),
+          );
+        },
+        child: const Text('OK'),
+      )
+    ],
+  );
+}
